aee92f00f0e0f2a0ea31304ef21051a3fc536845
Benjamin Harcourt <Benjamin Harcourt>
2016-07-14 15:47:58 -0400

    adding booleans to library

---
+++ booleans.js
@@ -0,0 +1,25 @@
+var age=24;
+var name="Ben ";
+var lastName="Harcourt";
+var feeling="STRESSED";
+console.log(feeling);
+console.log(age);
+
+var one =1;
+var stringOne="1";
+console.log(one==stringOne)
+// == means same value
+//=== means same value and TypeError
+
+
+console.log((4>3)&&(4<3));
+
+//&& = and
+
+console.log((4>3)||(4<3));
+
+// || = or
+
+console.log(4!=3)
+
+//! = not
