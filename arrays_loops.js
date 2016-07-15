602d96728c359c8ea56640c843982fab9c6b33c9
Benjamin Harcourt <Benjamin Harcourt>
2016-07-15 15:03:06 -0400

    adding to library

---
+++ arrays_loops.js
@@ -0,0 +1,8 @@
+var hamiltonCounty=["Noblesville", "Cicero", "Carmel", "Fishers", "Westfield"];
+for(hamiltonCounty=0;hamiltonCounty<5;hamiltonCounty++);
+	console.log(hamiltonCounty.length);
+//^array value (5 items)
+//v removing items from array and displaying them as strings
+var hamiltonCountyCities=["Noblesville", "Cicero", "Carmel", "Fishers", "Westfield"];
+for(i=0;i<5;i++);
+	console.log(hamiltonCountyCities);
---
+++ object_properties.js
@@ -0,0 +1,19 @@
+var fishersYelpRestraunts = {
+	directoryIdByZip	: 46038,
+	directoryName		:"Local Yelp Directory",
+	directoryDefined	:"This is a directory of restraunts in Fishers",
+	directoryRestruants : {
+		restrauntInfo		: [
+			{restrauntId : 1, name: "Bean Lean", rating: 3, topDish: "Leaning Bean Seam"},
+			{restrauntId : 2, name: "Skewer Sewer", rating: 1, topDish: "Opossum Blossum"},
+			{restrauntId : 3, name: "Sipable Stew", rating: 4, topDish: "Mean Bean Salad"},
+			{restrauntId : 4, name: "Ricky Racoons", rating: 5, topDish: "Trash Can BBQ"},
+			{restrauntId : 5, name: "The Corn Horn", rating: 2, topDish: "Classic Cob"}
+		]
+	}
+
+};
+for(var restraunt in fishersYelpRestraunts.directoryRestruants.restrauntInfo){
+	console.log(fishersYelpRestraunts.directoryRestruants.restrauntInfo[restraunt].name);
+}
+//copied and pasted from relp.it project to remind myself.
