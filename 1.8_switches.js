276e0cf120a3560b9205d7b34ddc8c3d4eb0f3f9
Benjamin Harcourt <Benjamin Harcourt>
2016-07-26 13:46:31 -0400

    added switches to library

---
+++ 1.8_switches.js
@@ -0,0 +1,35 @@
+var menuItem="";
+var ingredients; 
+
+switch(ingredients){
+	case "Taco Salad":
+		ingredients= "Seasoned beef over lettuce. Topped with chips, cheese, sour cream and salsa of choice";
+		break;
+	case "Steak Fajitas":
+		ingredients= "Shredded steak, rice, pinto beans, onions and peppers inside of a flour tortilla,";
+		break;
+	case "Shrimp Tacos":
+		ingredients= "Grilled cajun seasoned shrimp with shredded lettuce, cheese and salsa of choice inside of a hard shell";
+		break;
+}
+console.log(ingredients);
+
+//this is an example of a switch that could be used at a Mexican Restraunt
+var x="";
+var y;
+
+switch(y){
+	case /*x*/"____":
+		var y="_____";
+		break;
+	case /*x*/"____":
+		var y="_____";
+		break;
+	case /*x*/"____":
+		var y="_____";
+		break;
+	case /*x*/"____":
+		var y="_____";
+		break;
+}
+console.log(y);
