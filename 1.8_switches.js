747d022c9f3bd8094c3d880bd48714cbe178078e
Benjamin Harcourt <Benjamin Harcourt>
2016-07-26 13:57:32 -0400

    added another switch example

--- 1.8_switches.js
+++ 1.8_switches.js
@@ -33,3 +33,44 @@ switch(y){
 		break;
 }
 console.log(y);
+
+//this is a switch with a random function that I made with Joey
+var Horror = ["The Birds", "Psycho","The Shining"];
+var Comedy = ["Zoolander", "Hear No Evil, See No Evil", "Blazin' Saddles"];
+var Action = ["Shooter", "Saving Private Ryan", "The Boondock Saints"];
+var rand2;
+var randMov;
+var lastMovie;
+
+
+
+$(function(){
+	$("#random").click(function(){
+
+		do{
+			var rand1 = Math.floor((Math.random() * 3));
+		} while(rand1 === lastMovie);
+		
+		console.log(rand1);
+		switch(rand1)
+		{
+			case 0:		
+			rand2 = Math.floor((Math.random() * 3));
+			randMov=Horror[rand2];
+			break;
+
+			case 1:
+			rand2 = Math.floor((Math.random() * 3));
+			randMov=Comedy[rand2];
+			break;
+
+			case 2:
+			rand2 = Math.floor((Math.random() * 3));
+			randMov=Action[rand2];
+			break;
+
+		}
+		lastMovie = rand1;
+		$("#movie").val(randMov);
+	});
+});
