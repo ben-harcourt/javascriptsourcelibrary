bcba56cd9e7c0a0a37f0c83c92b93c0ad5d45ca9
Benjamin Harcourt <Benjamin Harcourt>
2016-07-27 11:10:01 -0400

    added prototypes to javascriptlibrary

---
+++ 2.2prototypes.js
@@ -0,0 +1,16 @@
+//this is a prototype (to my understanding)
+
+car={
+	make:"",
+	model:"",
+	mpg:"",
+	color:"",
+};
+//^^^this is a prototype for creating cars
+
+create.car={
+	make:"Dodge",
+	model:"Ram 2500",
+	mpg:"12/17",
+	color:"Green"
+};
