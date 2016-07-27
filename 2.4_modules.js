3213a2243cbb8c390c2e110cda8696bde9c78c0c
Benjamin Harcourt <Benjamin Harcourt>
2016-07-27 16:11:17 -0400

    added modules to my javascript library

---
+++ 2.4_modules.js
@@ -0,0 +1,12 @@
+var thisThat=function yippieKiyay(){
+	console.log("move along little doggy");
+};
+
+var Thatthis=function yeehaw(){
+	console.log("this town ain't big enough for the two of us");
+};
+
+//^^^this is a module of cowboy functions for cowboy coding.
+
+
+//Modules are folders and files to run large, usually prewritten code within your API 
