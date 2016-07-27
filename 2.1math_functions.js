3d064a7c1c597aaf493d5528123506ea91ad0e07
Benjamin Harcourt <Benjamin Harcourt>
2016-07-27 10:09:40 -0400

    started a math function page. will always add to this, there are a lot of them

---
+++ 2.1math_functions.js
@@ -0,0 +1,6 @@
+//Random
+math.random(); //will give a random number between 0 and 1
+math.random(x*10); //will give you a random number between 1 and 9
+math.sqrt(25); //will give the square root of a number (will return 5)
+math.ceil(45.4); //will return the smallest integer greater than or equal to a number (will return 46)
+math.floor(9.99); //will return the largest integer greater than or equal to a  (will return 9)
