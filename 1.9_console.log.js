e27b2b6373244f81d312f93f5274bfb1ec79055e
Benjamin Harcourt <Benjamin Harcourt>
2016-07-27 09:52:25 -0400

    added console.log and comparison operators to my open source library

---
+++ 1.9_console.log.js
@@ -0,0 +1,21 @@
+//although standard, I still found this to be an important piece to have in my library
+
+//Prints text to the console, useful for debugging
+ //start with variable and log the variable to send it to the console.
+
+ var myName="Benjamin Harcourt";
+ console.log(myName);
+
+ //run functions in the console
+var x=7;
+var y=6;
+function Example(x,y){
+	console.log(x+y);
+}
+
+//produce strings in the console. 
+var b="Benjamin ";
+var h="Harcourt";
+function myName(b,h){
+	console.log(b+h);
+}
---
+++ 2.0comparison_operators.js
@@ -0,0 +1,12 @@
+//this could potentially be used to give a user specified greetings
+
+x===y; //return s true if var x and var y are equal (four and 4 are not equals.).
+x!==y; //is true if they are not equal (four and 4 would be true in this case.).
+x<=y; //x is less than or equal to y
+x<y; //x is less than y
+x>y; //x is greater than y
+
+/*in my example, this could be used to greet users on a forum or website depending on their frequency to the site
+they could get something as simple as a "Welcome back!" if it recognized their IP adress or user information, 
+or it could be used to say "You've been visiting us quite a bit lately!" if it sees their IP address or user
+information has logged onto the server a certain number of times in an alotted time frame for the server*/
